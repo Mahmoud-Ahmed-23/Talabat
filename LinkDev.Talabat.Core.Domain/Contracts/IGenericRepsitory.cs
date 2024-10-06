17f5cf89d59738bc5b725dc0f965f9ccffebe69b
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2024-10-06 23:46:27 +0300

    Generic Repository

---
+++ LinkDev.Talabat.Core.Domain/Contracts/IGenericRepsitory.cs
@@ -0,0 +1,23 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Contracts
+{
+	public interface IGenericRepsitory<TEntity, TKey> where TEntity : BaseEntity<TKey> where TKey : IEquatable<TKey>
+	{
+
+		Task<IEnumerable<TEntity>> GetAllAsync(bool withTraking);
+
+		Task<TEntity> GetAsync(TKey id);
+
+		Task AddAsync(TEntity entity);
+
+		void Update(TEntity entity);
+
+		void Delete(TEntity entity);
+	}
+}
